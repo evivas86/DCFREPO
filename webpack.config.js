2c7bf9b9a9a36df724c55be276465776d90fa57a
Veado <veado@protonmail.com>
2022-03-29 21:53:57 +0200

    Remove extra config for webpack, use `browser`
    in package.json

--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = (config) => {
-  config.resolve.fallback = {
-    "stream": require.resolve("stream-browserify"),
-    "crypto": require.resolve("crypto-browserify")
-  }
-  return config;
-};
